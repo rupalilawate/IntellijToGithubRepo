c63e74523fd3336074e1b48267cd309bb0a11d43
rupali8080 <rupalilawate@gmail.com>
2024-11-16 23:09:26 +0530

    First Commit

---
+++ src/test/java/SeleniumTestCases/UITestcase.java
@@ -0,0 +1,31 @@
+package SeleniumTestCases;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class UITestcase {
+
+
+    @Test
+    public void startBrowser() {
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://tutorialsninja.com/demo/");
+        Assert.assertTrue(driver.getTitle().contains("Your Store"), "Title does not match");
+        System.out.println("test successfully passed");
+        driver.quit();
+    }
+
+
+
+
+
+}
