b5700cea05628b8053fa2887796ce005039241f2
rupali8080 <rupalilawate@gmail.com>
2024-11-21 15:16:25 +0530

    parameter added

--- src/test/java/SeleniumTestCases/UITestcase.java
+++ src/test/java/SeleniumTestCases/UITestcase.java
@@ -3,7 +3,9 @@ package SeleniumTestCases;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -11,11 +13,21 @@ import java.time.Duration;
 public class UITestcase {
 
 
+    @Parameters("Browser")
     @Test
-    public void startBrowser() {
-        WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void startBrowser(String browserName) {
+
+        System.out.println("Parameter value is: " + browserName);
+
+        WebDriver driver = null;
+        if(browserName.contains("Chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else if(browserName.contains("Edge")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://tutorialsninja.com/demo/");
